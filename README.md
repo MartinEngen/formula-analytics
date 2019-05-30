4f1d99af2a09c6091189e7f762fe1c9f4e3f78f3
MartinEngen <martin.engen@item.no>
2019-05-30 16:23:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## FormulaOne Analytics
+This project is intended as a hobby project to deepen knowledge in Play Framework, Vue and D3.
+
+##
+
+## This project setup was forked from
+
+[Play Framework + Vue.js starter](https://github.com/gbogard/play-vue-webpack).
+
+Install the front-end dependencies
+
+```
+cd front && npm install
+```
+
+Launch the project
+
+```
+sbt ~run
+```
+
+For more details, see the Play Framework + Vue.js starter project.
---
+++ front/src/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    </div>
+</template>
+
+<script>
+    import * as d3 from "d3";
+
+
+    export default {
+        name: "app",
+        components: {},
+        data: function() {
+            return  {
+                count: 0
+            }
+        },
+        mounted() {
+        },
+        methods: {
+
+        }
+    };
+</script>
+
+<style>
+
+</style>
